f734adacba8649c2ff75d78a95f1a83c5bbf2f70
tenpei999 <tenpei999@gmail.com>
2024-03-10 12:45:40 +0900

    fiest_build

---
+++ recruite1/dist/assets/main.js
@@ -0,0 +1,28 @@
+const input = "";
+const main = "";
+window.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".js-hamburger");
+  const body = document.querySelector("body");
+  const nav = document.querySelector(".p-nav");
+  const navBody = document.querySelector(".p-nav__body");
+  const navButton = document.querySelector(".p-nav__button");
+  const buttonSquare = document.querySelector(".c-button-square");
+  document.querySelector(".header-logo");
+  const toggleMenu = () => {
+    body.classList.toggle("is-open");
+    nav.classList.toggle("is-open");
+    navBody.classList.toggle("is-open");
+    navButton.classList.toggle("is-open");
+    buttonSquare.classList.toggle("is-open");
+  };
+  hamburger.addEventListener("click", toggleMenu);
+  document.addEventListener("click", (e) => {
+    if (body.classList.contains("is-open") && !hamburger.contains(e.target)) {
+      toggleMenu();
+    }
+  });
+});
+function changeText() {
+  console.log("hogera");
+}
+document.addEventListener("DOMContentLoaded", changeText);
